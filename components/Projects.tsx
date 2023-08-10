7ec6fe5e3d350ee33ab3254015203bdc2ec6e50c
JonathanDR2000 <jonathandr2000@gmail.com>
2023-08-10 22:14:42 +0200

    upload2

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import Link from 'next/link';
 
 type Props = {}
 
 export default function Projects({}: Props) {
-    const projects = [1,2,3,4,5];
+    const projects = [1,2,3,4,5,6,7,8,9,10,11,12];
   return (
     <motion.div 
     initial={{ opacity: 0,}}
@@ -219,6 +220,7 @@ plemented in a game
                    
                 </div>
                 <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'> 
+                <Link href='https://vimeo.com/403338365'>
                   <motion.img className='xl:w-[900px] xl:h-[900px]'
                    initial={{ y: -300, opacity: 0,}}
                    transition={{duration: 1.2,}}
@@ -227,6 +229,8 @@ plemented in a game
                     src='/Captura7.png'
                     alt=''
                   />
+                  </Link>
+                  
                   <div className='space-y-10 px-10 md:px-10 max-w-6xl'>
                     <h4 className='text-4xl font-semibold text-center'>
                       <span className='underline decoration-[#F7AB0A]/50'>
