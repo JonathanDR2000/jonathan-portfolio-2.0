296345d7ce24821f2bbd9463a08bbfb03ed54dbf
JonathanDR2000 <jonathandr2000@gmail.com>
2023-08-25 19:04:11 +0200

    changes18

--- components/Skill9.tsx
+++ components/Skill9.tsx
@@ -16,13 +16,13 @@ export default function Skill9({ directionLeft}: Props) {
         transition={{duration:1}}
         whileInView={{opacity: 1, x: 0}}
         src='/ZBrush-logo-43D6324DC8-seeklogo.com.png'
-        className='rounded-full border border-gray-500 object-cover w-16 h-16 sm:w-24 sm:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter 
+        className='rounded-full border border-gray-500 object-cover w-14 h-14 sm:w-24 sm:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter 
         group-hover:grayscale transition
         duration-300 ease-in-out'
         alt=''
         />
         <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out
-        group-hover:bg-white w-16 h-16 sm:w-24 sm:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full'>
+        group-hover:bg-white w-14 h-14 sm:w-24 sm:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full'>
             <div className='flex items-center justify-center h-full'>
                 <p className='text-3xl font-bold text-black opacity-100'>
                     60%
